477c859c7d3f96c1ea2d97ad7d62400e7b56d637
nonylene <cookie.speaker@gmail.com>
2016-09-18 01:03:10 +0900

    add mysql dump to slack integration

--- recipe.rb
+++ recipe.rb
@@ -6,7 +6,11 @@ define :setup_user, user_name: nil, key_source: nil do
   user define_params[:user_name] do
     password PASSWORD
     create_home true
+  end
+
+  user define_params[:user_name] do
     shell "/bin/bash"
+    only_if "bash -c '[[ `getent passwd #{define_params[:user_name]}` == */bin/sh* ]]'"
   end
 
   directory "/home/#{define_params[:user_name]}/.ssh" do
@@ -43,3 +47,41 @@ template "/etc/sudoers.d/neko-sudoers" do
 end
 
 %w(zsh tmux git tig vim p7zip-full).each {|pack| package pack}
+
+
+# dumps
+directory "/opt/bin" do
+  owner "root"
+  group "root"
+  mode  "777"
+end
+
+directory "/var/log/mysql/old/" do
+  owner "root"
+  group "root"
+  mode  "777"
+end
+
+directory "/var/log/mysql/slow-query/" do
+  owner "root"
+  group "root"
+  mode  "777"
+end
+
+directory "/var/log/nginx/" do
+  mode  "755"
+end
+
+directory "/var/log/nginx/old/" do
+  owner "root"
+  group "root"
+  mode  "777"
+end
+
+template "/opt/bin/dump-slow-and-notify.sh" do
+  source "templates/dump-slow-and-notify.sh.erb"
+  mode   "775"
+  owner  "root"
+  group  "root"
+  variables(slack_channels: SLACK_CHANNELS, slack_token: SLACK_TOKEN)
+end
